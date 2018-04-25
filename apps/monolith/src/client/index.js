127aa704aac853a40cb9f6a5a6640fa6c6e6d9ac
Walee Ahmed <dev@walee.me>
2018-04-24 23:13:03 -0400

    initial commit 🎉

---
+++ apps/monolith/src/client/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+import { hydrate } from "react-dom"
+import App from "../common/App"
+
+hydrate(<App />, document.getElementById("root"))
+
+if (module.hot) module.hot.accept()
