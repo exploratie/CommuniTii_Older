4a9b244b99e08318188e2f928382bae3724eeda0
Walee Ahmed <dev@walee.me>
2018-05-12 00:33:50 -0400

    removed mongo prebuilt and reverted back to cloud mongodb for development

--- apps/monolith/src/server/lib/initMongo.js
+++ apps/monolith/src/server/lib/initMongo.js
@@ -2,24 +2,10 @@ import { MongoConnector, MongoEntity } from "apollo-connector-mongodb"
 import lruCache from "lru-cache"
 
 export default async () => {
-  const isDev = process.env.NODE_ENV === "development"
-
-  const mongoURL = isDev
-    ? "mongodb://localhost:4000"
-    : `
-    mongodb://
-    ${process.env.RAZZLE_MONGO_USER}:
-    ${process.env.RAZZLE_MONGO_PASSWORD}@
-    ${process.env.RAZZLE_MONGO_LINK}`
-
   try {
-    if (isDev) {
-      const { MongodHelper } = await import("mongodb-prebuilt")
-      const mongodHelper = new MongodHelper(["--port", "4000"])
-
-      await mongodHelper.run()
-      console.log(`➡️  Started MongoDB for Development at ${mongoURL}`)
-    }
+    const mongoURL = `mongodb://${process.env.RAZZLE_MONGO_USER}:${
+      process.env.RAZZLE_MONGO_PASSWORD
+    }@${process.env.RAZZLE_MONGO_LINK}`
 
     const conn = new MongoConnector(mongoURL)
     await conn.connect()
